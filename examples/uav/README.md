61ba017c6c558b7cb15b88a20061e8b6d1991165
Ramon Fontes <ramonreisfontes@gmail.com>
2021-01-14 21:12:03 -0300

    first commit

--- examples/uav/README.md
+++ examples/uav/README.md
@@ -1,22 +1,20 @@
-# Example of Drones integration using CoppeliSim
+# Example of Drones integration using CoppeliaSim
 
 Download CoppeliSim, you can use the script:
 
-		./examples/uav/install.sh
+```
+./examples/uav/install.sh
+```
 
 ## Centralized and adhoc modes
 
 This example can run as a centralized or adhoc mode. 
 
-Start mininet-wifi and select the corresponding mode as an argument (e.g., centralized, adhoc). 
+Start the network ad hoc scenario with the command below:
 
-For example, you can run the centralized scenario with the command:
-
-		sudo python examples/uav/drone.py centralized
-
-Or the adhoc scenario with the command:
-
-		sudo python examples/uav/drone.py adhoc
+```
+sudo python examples/uav/drone.py
+```
 
 This will start mn-wifi and the simulation in CoppeliaSim.
 
@@ -24,12 +22,13 @@ In parallel, it will start the remote API capturing the drones' position in the 
 
 In the mininet graph, you can see that the stations' position is updated according to the drones.
 
-To verify the Drones' position in the simulation, run the getstaposition.py script with the station name (Only one station supported).
+To verify the Drones' position in the simulation, run the `getstaposition.py` script with the station name (Only one station supported).
 
 For example, to verify the position of sta1 (Drone1) run:
 
-		sudo python examples/uav/getstaposition.py sta1		
-
+```
+sudo python examples/uav/getstaposition.py sta1		
+```
 To see the position of all the drones, no pass any argument.
 
 
@@ -38,9 +37,13 @@ To see the position of all the drones, no pass any argument.
 
 If there is an error to connect to CoppeliaSim socket, run:
 		
-		sudo pkill -9 -f python
+```
+sudo pkill -9 -f python
+```
 
 If CoppeliaSim continue runing after finishing mininet-wifi execution, run:
 
-		sudo pkill -9 -f coppeliaSim
+```
+sudo pkill -9 -f coppeliaSim
+```
 ---
--- examples/uav/drone.py
+++ examples/uav/drone.py
@@ -11,80 +11,46 @@ import time
 import os
 import sys
 
-def topology(args):
 
-    modes = ['centralized', 'adhoc']
-    validmode = 0
-    sta_drone = []
+def topology():
 
-    if len(args) > 1:
-        mode = str(args[1])
-    else:
-        print("No mode defined")
-        exit()
-
-    for mode_val in args:
-        if mode_val in modes:
-            mode = mode_val
-            validmode = 1
-
-    if validmode == 0:
-        print("Mode not supprted, try centralized or adhoc")
-        exit()
-
-    net = Mininet_wifi()
+    "Create a network."
+    net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
 
     info("*** Creating nodes\n")
-    sta1 = net.addStation('sta1', mac='00:00:00:00:00:02', ip='10.0.0.2/8',
+    dr1 = net.addStation('dr1', mac='00:00:00:00:00:02', ip='10.0.0.2/8',
                    position='30,60,0')
-    sta2 = net.addStation('sta2', mac='00:00:00:00:00:03', ip='10.0.0.3/8',
+    dr2 = net.addStation('dr2', mac='00:00:00:00:00:03', ip='10.0.0.3/8',
                    position='70,30,0')
-
-    sta3 = net.addStation('sta3', mac='00:00:00:00:00:04', ip='10.0.0.4/8',
+    dr3 = net.addStation('dr3', mac='00:00:00:00:00:04', ip='10.0.0.4/8',
                    position='10,20,0')
-
-    if mode == 'centralized':
-        print("Centralized mode")
-        ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1',
-                                 failMode="standalone", position='50,50,0')
 
     net.setPropagationModel(model="logDistance", exp=4.5)
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
 
+    net.addLink(dr1, cls=adhoc, intf='dr1-wlan0',
+            ssid='adhocNet', proto='batman_adv',
+            mode='g', channel=5, ht_cap='HT40+')
 
-    if mode == 'adhoc':
-        print("adhoc mode")
-        net.addLink(sta1, cls=adhoc, intf='sta1-wlan0',
-                ssid='adhocNet', proto='batman_adv',
-                mode='g', channel=5, ht_cap='HT40+')
+    net.addLink(dr2, cls=adhoc, intf='dr2-wlan0',
+            ssid='adhocNet', proto='batman_adv',
+            mode='g', channel=5, ht_cap='HT40+')
 
-        net.addLink(sta2, cls=adhoc, intf='sta2-wlan0',
-                ssid='adhocNet', proto='batman_adv',
-                mode='g', channel=5, ht_cap='HT40+')
-
-        net.addLink(sta3, cls=adhoc, intf='sta3-wlan0',
-                ssid='adhocNet', proto='batman_adv',
-                mode='g', channel=5, ht_cap='HT40+')
+    net.addLink(dr3, cls=adhoc, intf='dr3-wlan0',
+            ssid='adhocNet', proto='batman_adv',
+            mode='g', channel=5, ht_cap='HT40+')
 
     info("*** Starting network\n")
-    if mode == 'centralized':
-        net.addNAT(linkTo='ap1').configDefault()
     net.build()
-    if mode == 'centralized':
-        ap1.start([])
 
-    if mode == 'centralized':
-        nodes = net.stations + net.aps
-    if mode == 'adhoc':
-        nodes = net.stations
-
+    nodes = net.stations
     telemetry(nodes=nodes, single=True, data_type='position')
 
+    sta_drone = []
     for n in net.stations:
         sta_drone.append(n.name)
-
     sta_drone_send = ' '.join(map(str, sta_drone)) 
 
     # # set_socket_ip: localhost must be replaced by ip address
@@ -93,36 +59,35 @@ def topology(args):
     info("*** Starting Socket Server\n")
     net.socketServer(ip='127.0.0.1', port=12345)
 
-    info("*** Running CLI\n")
-    
+    info("*** Starting CoppeliaSim\n")
     os.system('./examples/uav/CoppeliaSim_Edu_V4_1_0_Ubuntu18_04/coppeliaSim.sh -s examples/uav/simulation.ttt -gGUIITEMS_2 &')
     time.sleep(10)
 
-    simpleTest = 'sudo python examples/uav/simpleTest.py ' + str(sta_drone_send) + ' &'
+    info("*** Perform a simple test\n")
+    simpleTest = 'python examples/uav/simpleTest.py ' + str(sta_drone_send) + ' &'
     os.system(simpleTest)
 
-    setstaposition = 'sudo python examples/uav/setstaposition.py ' + str(sta_drone_send) + ' &'
+    info("*** Configure the node position\n")
+    setstaposition = 'python examples/uav/setstaposition.py ' + str(sta_drone_send) + ' &'
     os.system(setstaposition)
 
+    info("*** Running CLI\n")
     CLI(net)
 
     info("*** Stopping network\n")
-
     kill_process()
     net.stop()
 
 
 def kill_process():
-    os.system('sudo pkill -9 -f coppeliaSim')
-    os.system('sudo pkill -9 -f simpleTest.py')
-    os.system('sudo pkill -9 -f setstaposition.py')
+    os.system('pkill -9 -f coppeliaSim')
+    os.system('pkill -9 -f simpleTest.py')
+    os.system('pkill -9 -f setstaposition.py')
 
 
-    
 if __name__ == '__main__':
     setLogLevel('info')
     
     # Killing old processes
     kill_process()
-
-    topology(sys.argv)
+    topology()
